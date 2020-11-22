79a9719f203bd94b74460152d621ed2543cae380
André <absilva@emancipa.online>
2020-11-22 17:22:49 -0300

    fish intercative sessions:
    define a simple prompt for fish;
    don't show greeting message for new sessions.

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,4 +1,15 @@
+function fish_greeting
+end
+
+function fish_prompt
+  set -g fish_prompt_pwd_dir_length 3
+
+  set_color    green  ; echo -ne "@"               ; \
+  set_color -i blue   ; echo -ne (hostname) ""     ; \
+  set_color    normal ; echo -ne (prompt_pwd) "\n" ; \
+  set_color    green  ; echo -ne 'λ.'
+end
+
 for file in $__fish_config_dir/config.d/*.fish
   source $file
 end
-
