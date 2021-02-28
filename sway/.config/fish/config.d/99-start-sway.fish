8073f46c3bb0900e60a4c566978fc37c648c1ce9
André <abs@emancipe.se>
2021-02-27 21:13:19 -0300

    remove old DE config files

--- sway/.config/fish/config.d/99-start-sway.fish
+++
@@ -1,6 +0,0 @@
-if status --is-login; and test (tty) = "/dev/tty1"
-  systemctl --user unset-environment SWAYSOCK WAYLAND_DISPLAY
-  systemctl --user import-environment XDG_SEAT XDG_SESSION_ID XDG_VTNR
-  exec systemctl --wait --user start sway
-end
-
